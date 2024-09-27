2a2623cfb0ef4c0fa100f364d52812704363c0e8
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2024-09-27 10:17:11 +0530

    add: dicedb connection code

--- internal/db/dicedb.go
+++ internal/db/dicedb.go
@@ -3,3 +3,34 @@ this will be the DiceDB client
 */
 
 package db
+
+import (
+	"context"
+	"log"
+
+	dice "github.com/dicedb/go-dice"
+)
+
+var rdb *dice.Client
+var ctx = context.Background()
+
+func InitializeDice(){
+	rdb = dice.NewClient(&dice.Options{
+		Addr: "localhost:7379",
+		Password: "",
+		DB: 0,
+	})
+
+	err := rdb.Ping(ctx).Err()
+	if err != nil{
+		log.Fatalf("error connecting to DiceDB: %v", err)
+	}
+	log.Println("connected to DiceDB")
+}
+
+func CloseDiceDB(){
+	err := rdb.Close()
+	if err!=nil{
+		log.Fatalf("error closing DiceDB connection: %v", err)
+	}
+}
