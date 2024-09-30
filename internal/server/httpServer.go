bc459e278502476001e2160297c58d9d97b07e7d
tanbirali <tanbirali2004@gmail.com>
2024-09-30 21:21:43 +0530

    feat: implemented the search api with a static map

--- internal/server/httpServer.go
+++ internal/server/httpServer.go
@@ -100,6 +100,49 @@ func (s *HTTPServer) SearchHandler(w http.ResponseWriter, request *http.Request)
 	if q == "*" {
 		q = ""
 	}
+	commands := map[string]map[string]string {
+		
+			"SET": {
+			  "title": "<b>SET</b>",
+			  "syntax": "SET key value [NX | XX] [EX seconds | PX milliseconds | EXAT unix-time-seconds | PXAT unix-time-milliseconds | KEEPTTL ]",
+			  "body": "The SET command in DiceDB is used to set the value of a key. If the key already holds a value, it is overwritten, regardless of its type. This is one of the most fundamental operations in DiceDB as it allows for both creating and updating key-value pairs.",
+			  "url": "https://dicedb-docs.netlify.app/commands/set/",
+			},
+			"GET": {
+			  "title": "<b>GET</b>",
+			  "syntax": "GET key",
+			  "body": "The GET command retrieves the value of a key. If the key does not exist, nil is returned.",
+			  "url": "https://dicedb-docs.netlify.app/commands/get/",
+			},	  
+	}
+	matchingCommands := []map[string]string{}
+	for _, command := range commands {
+		
+		title, okTitle := command["title"]
+		body, okBody:= command["body"]
+		if okTitle && okBody {
+			if strings.Contains(strings.ToLower(title), q) ||
+			 strings.Contains(strings.ToLower(body), q) {
+
+			highlightedText := strings.ReplaceAll(title, q, "<b>"+q+"</b>")
+
+			matchingCommands = append(matchingCommands, map[string]string{
+				"title": highlightedText,
+				"syntax": command["syntax"],
+				"body": body,
+				"url": command["url"],
+			} )
+			}
+		}
+	}
+	if len(matchingCommands) == 0 {
+		util.JSONResponse(w, http.StatusOK, map[string]string{"message": "No search results"})
+		return
+	}
+	response := map[string]interface{}{
+		"total": len(matchingCommands),
+		"results": matchingCommands,
+	}
 	
-	util.JSONResponse(w, http.StatusOK, map[string]string{"message": "Search results"})
+	util.JSONResponse(w, http.StatusOK, response)
 }
