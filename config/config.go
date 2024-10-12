bd0d12bde5c850dfa23c511574d3de9904eb7520
Shimanshu Yadav <shimanshuyadav83@gmail.com>
2024-10-12 17:11:43 +0530

    added 15 min default for CleanupCronFrequency config

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ func LoadConfig() *Config {
 		RequestLimitPerMin:   getEnvInt("REQUEST_LIMIT_PER_MIN", 1000),                          // Default request limit
 		RequestWindowSec:     getEnvFloat64("REQUEST_WINDOW_SEC", 60),                           // Default request window in float64
 		AllowedOrigins:       getEnvArray("ALLOWED_ORIGINS", []string{"http://localhost:3000"}), // Default allowed origins
-		CleanupCronFrequency: getEnvInt("REQUEST_LIMIT_PER_MIN", 5*1),
+		CleanupCronFrequency: getEnvInt("REQUEST_LIMIT_PER_MIN", 60*15),
 	}
 }
 
